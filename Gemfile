573d41aecccef75a8b4143e8b57d70b57b9bb61b
yuki ohishi <yukiohishi@yukinoMacBook-Air.local>
2021-02-07 07:18:39 +0900

    sqlite to mysql

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ ruby '2.7.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.1.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+gem 'sqlite3', group: :development
+# Use mysql as the database for Active Record
+gem 'mysql2', '~> 0.5', group: :production
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+data = [
+    {
+        title:'チキンカレー',
+        making_time:'45分',
+        serves:'4人',
+        ingredients:'玉ねぎ,肉,スパイス',
+        cost:1000
+    },
+    {
+        title:'オムライス',
+        making_time:'30分',
+        serves:'2人',
+        ingredients:'玉ねぎ,卵,スパイス,醤油',
+        cost:700
+    }
+]
+
+recipes = Recipe.create(data)
